52951d962c9e020ea7cc30066072fa62dbb0fb0b
Agustín Tisera <agustint@nimble.la>
2020-10-28 14:04:25 -0300

    Simple verify password tests

--- functions/index.js
+++ functions/index.js
@@ -4,6 +4,15 @@ const getMaxFromList = numbers => numbers.length === 0 ? null : Math.max(...numb
 // Función para validar si un número es par
 
 // Función que valide un password (sólo longitud y que sea alfanumérico)
+const verifyPassword = password => {
+  const checkLength = stringPassword => stringPassword.length >= 8 ? true : false;
+
+  const checkAlpha = stringPassword =>
+    stringPassword.match(/([a-z]+[0-9])|([0-9]+[a-z])/i) ? true : false;
+
+  if (password && checkAlpha(password) && checkLength(password)) return true;
+  else return false;
+};
 
 // Función que reciba una lista de números que pueden ser repetidos pero no consecutivos
 
@@ -15,4 +24,5 @@ const getMaxFromList = numbers => numbers.length === 0 ? null : Math.max(...numb
 
 module.exports = {
   getMaxFromList,
+  verifyPassword,
 };
--- functions/index.test.js
+++ functions/index.test.js
@@ -1,4 +1,5 @@
 const functions = require('./index');
+const { verifyPassword } = require('./index');
 
 describe('getMaxFromList', () => {
   it('should return null if list is empty', () => {
@@ -14,6 +15,47 @@ describe('getMaxFromList', () => {
 
     tests.forEach(({ numbers, expected }) => {
       expect(functions.getMaxFromList(numbers)).toBe(expected);
+    });
+  });
+});
+
+describe('Verify password', () => {
+  it('Should check the minimum length of 8 and alphaNumeric conditions', () => {
+    const tests = [
+      { password: undefined, expected: false},
+      { password: '', expected: false },
+      { password: 'less8', expected: false },
+      { password: 'moreThanEightBuNotAlpha', expected: false },
+      { password: 'LimitEi8', expected: true },
+      { password: 'thisIsOkPassword8', expected: true },
+    ];
+
+    tests.forEach(({ password, expected }) => {
+      expect(functions.verifyPassword(password)).toBe(expected);
+    });
+  });
+  describe('Alternative way', () => {
+    // Alternative: We write more code but it's easier to find and debug the failing ones.
+    it('Should reject empty strings', () => {
+      expect(verifyPassword('')).toBe(false);
+    });
+
+    it('Should reject undefined passwords', () => {
+      expect(verifyPassword()).toBe(false);
+    });
+
+    it('Should reject passwords with length minor than 8', () => {
+      expect(verifyPassword('less8')).toBe(false);
+    });
+
+    it('Should reject non alphanumeric passwords', () => {
+      expect(verifyPassword('moreThanEightButNotAlpha')).toBe(false);
+      expect(verifyPassword('111122224444')).toBe(false);
+    });
+
+    it('Should admit alphanumeric passwords with length of 8 or more', () => {
+      expect(verifyPassword('thisIsOkPassword8')).toBe(true);
+      expect(verifyPassword('thisIs88')).toBe(true);
     });
   });
 });
